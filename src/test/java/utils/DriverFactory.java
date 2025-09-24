cce82f33db242d3ac0a89d6d6b82c90a8ef9f091
enoquejonas <enoque.macanda@gmail.com>
2025-09-24 12:04:30 +0200

    Update DriverFactory.java

--- src/test/java/utils/DriverFactory.java
+++ src/test/java/utils/DriverFactory.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Parameters;
-
+import org.openqa.selenium.firefox.FirefoxOptions;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -12,19 +12,30 @@ public class DriverFactory {
     private static ThreadLocal<RemoteWebDriver> driver = new ThreadLocal<>();
 
     // Using a remote URL - the tests will run on a docker container (selenium/standalone-chrome)
-    private static final String REMOTE_URL = "http://localhost:4444";
+    private static final String REMOTE_URL =
+            System.getenv("SELENIUM_REMOTE_URL") != null
+                    ? System.getenv("SELENIUM_REMOTE_URL")
+                    : "http://localhost:4444/wd/hub";
 
     public static void initDriver(String browser) throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-        if(browser.equalsIgnoreCase("firefox")){
-            capabilities.setCapability("browserName", "firefox");
-        }
-        if(browser.equalsIgnoreCase("chromium")){
-            capabilities.setCapability("browserName", "chrome");
-        }
-        driver.set(new RemoteWebDriver(new URL(REMOTE_URL), capabilities));
-    }
+        switch (browser.toLowerCase()) {
+            case "firefox":
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                firefoxOptions.addArguments("--headless");
+                driver.set(new RemoteWebDriver(new URL(REMOTE_URL), firefoxOptions));
+                break;
 
+            case "chromium": // map chromium → chrome
+            case "chrome":
+            default:
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless=new");
+                chromeOptions.addArguments("--no-sandbox");
+                chromeOptions.addArguments("--disable-dev-shm-usage");
+                driver.set(new RemoteWebDriver(new URL(REMOTE_URL), chromeOptions));
+                break;
+        }
+    }
     public static RemoteWebDriver getDriver() {
         return driver.get();
     }
